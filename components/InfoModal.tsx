5edd2bd22b7b3e4bf7e1cf28551a5e662a345383
stephanusyogi <stephanusyogi12@gmail.com>
2023-11-21 16:52:13 +0700

    Netflix Clone

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -24,7 +24,7 @@ const InfoModal:React.FC<InfoModalProps> = ({visible, onClose}) => {
     setTimeout(() => {
       onClose()
     }, 300);
-  },[])
+  },[onClose])
 
   if (!visible) {
     return null
